96841f6a0d612c605dff2fb4236abc259cc67639
Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>
2023-11-22 11:40:55 +0200

    ref: modify ed25519 interface

--- src/crypto/ed25519.rs
+++ src/crypto/ed25519.rs
@@ -1,11 +1,11 @@
 
-use alloy_primitives::FixedBytes;
+use alloy_primitives::{FixedBytes, Bytes};
 use ed25519_compact::{PublicKey, Signature};
 
 
 /// Verifies an ed25519 signature.
-pub fn ed25519_verify(k: FixedBytes<32>, sig: FixedBytes<64>, msg: FixedBytes<32>) -> bool {
+pub fn ed25519_verify(k: FixedBytes<32>, sig: Bytes, msg: Bytes) -> bool {
     let pk: PublicKey = PublicKey::from_slice(k.as_slice()).unwrap();
-    let signature = Signature::from_slice(sig.as_slice()).unwrap();
+    let signature = Signature::from_slice(sig.as_ref()).unwrap();
     pk.verify(msg, &signature).is_ok()
 }
