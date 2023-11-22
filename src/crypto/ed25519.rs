f893a2b4daf0bafee4f5cabe63389ca26f853e31
Daniel-K-Ivanov <daniel.k.ivanov95@gmail.com>
2023-11-22 11:49:14 +0200

    feat: change bytes impl

--- src/crypto/ed25519.rs
+++ src/crypto/ed25519.rs
@@ -1,11 +1,12 @@
 
-use alloy_primitives::{FixedBytes, Bytes};
+use alloy_primitives::FixedBytes;
+use stylus_sdk::abi::Bytes;
 use ed25519_compact::{PublicKey, Signature};
 
 
 /// Verifies an ed25519 signature.
 pub fn ed25519_verify(k: FixedBytes<32>, sig: Bytes, msg: Bytes) -> bool {
     let pk: PublicKey = PublicKey::from_slice(k.as_slice()).unwrap();
-    let signature = Signature::from_slice(sig.as_ref()).unwrap();
+    let signature = Signature::from_slice(sig.as_slice()).unwrap();
     pk.verify(msg, &signature).is_ok()
 }
