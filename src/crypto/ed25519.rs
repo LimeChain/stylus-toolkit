29617755e922d8acb48f39b11d5459f4d8013448
daniel-k-ivanov <daniel.k.ivanov95@gmail.com>
2024-07-08 15:22:59 +0200

    update bytes usage

--- src/crypto/ed25519.rs
+++ src/crypto/ed25519.rs
@@ -1,11 +1,10 @@
 
 use alloy_primitives::FixedBytes;
-use stylus_sdk::abi::Bytes;
 use ed25519_compact::{PublicKey, Signature};
 
 
 /// Verifies an ed25519 signature.
-pub fn ed25519_verify(k: FixedBytes<32>, sig: Bytes, msg: Bytes) -> bool {
+pub fn ed25519_verify(k: FixedBytes<32>, sig: Vec<u8>, msg: Vec<u8>) -> bool {
     let pk: PublicKey = PublicKey::from_slice(k.as_slice()).unwrap();
     let signature = Signature::from_slice(sig.as_slice()).unwrap();
     pk.verify(msg, &signature).is_ok()
